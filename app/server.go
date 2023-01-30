25a0fc0a3e01339bdf021957c65531ee795552fa
Kevin <vargaskevv@gmail.com>
2023-01-30 14:38:30 -0400

    #12 stats service

--- app/server.go
+++ app/server.go
@@ -69,6 +69,7 @@ func (server Server) Run() error {
 	server.app.Get("/years", handlers.YearlyDataHandler(server.db))
 	server.app.Get("/static", handlers.StaticDataHandler(server.db))
 	server.app.Post("/errors", handlers.SaveAppErrors(server.db))
+	server.app.Get("/stats", handlers.StatsHandler(server.db))
 
 	protected := server.app.Group("/auth", authMiddleware(server.config))
 
