4a04d431211aefa7bdd91459ff8321f2a0d2a4e6
Kevin <vargaskevv@gmail.com>
2023-06-15 21:00:57 -0400

    Ignore database setup files from tests

--- runtest.sh
+++ runtest.sh
@@ -5,4 +5,4 @@ DB_USER=usertest \
 DB_PASSWORD=passtest \
 DB_SSL_MODE=disable \
 APP_PORT=4000 \
-go test $(go list ./... | grep -v /ent | grep -v /staticdata | grep -v /extra) -cover
+go test $(go list ./... | grep -v /ent | grep -v /staticdata | grep -v /extra | grep -v /database) -cover
