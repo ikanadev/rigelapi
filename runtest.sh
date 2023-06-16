9917cb256bbe8beb2871c9a89a247fb478b57f29
Kevin <vargaskevv@gmail.com>
2023-06-15 20:53:15 -0400

    udpate runtest.sh script to grap env variables

--- runtest.sh
+++ runtest.sh
@@ -1,2 +1,8 @@
-DB_NAME=dbtest DB_HOST=hosttest DB_PORT=porttest DB_USER=usertest DB_PASSWORD=passtest DB_SSL_MODE=disable APP_PORT=4000
-go test $(go list ./... | grep -v /ent | grep -v /staticdata | grep -v /extra)
+DB_NAME=dbtest \
+DB_HOST=hosttest \
+DB_PORT=porttest \
+DB_USER=usertest \
+DB_PASSWORD=passtest \
+DB_SSL_MODE=disable \
+APP_PORT=4000 \
+go test $(go list ./... | grep -v /ent | grep -v /staticdata | grep -v /extra) -cover
