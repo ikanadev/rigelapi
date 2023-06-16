75f07e499a2d3379fb5163d765cf910d26a1d119
Kevin <vargaskevv@gmail.com>
2023-06-15 20:53:23 -0400

    config tests

--- config/config_test.go
+++ config/config_test.go
@@ -8,7 +8,33 @@ import (
 )
 
 func TestGetEnvOrPanic(t *testing.T) {
-	os.Setenv("RANDOM", "RANDOM")
-	value := getEnvOrPanic("RANDOM")
-	assert.Equal(t, value, "RANDOM")
+	assert := assert.New(t)
+	strVal := "RANDOM"
+	os.Setenv(strVal, strVal)
+	value := getEnvOrPanic(strVal)
+	assert.Equal(strVal, value)
+	assert.Panics(func() {
+		getEnvOrPanic("INEXISTENT")
+	})
+}
+
+// DB_NAME=dbtest DB_HOST=hosttest DB_PORT=porttest DB_USER=usertest DB_PASSWORD=passtest DB_SSL_MODE=disable APP_PORT=4000
+func TestGetConfig(t *testing.T) {
+	assert := assert.New(t)
+	config := GetConfig()
+	expected := Config{
+		DB: dbData{
+			Host:     "hosttest",
+			Port:     "porttest",
+			Name:     "dbtest",
+			User:     "usertest",
+			Password: "passtest",
+			SslMode:  "disable",
+		},
+		App: appData{
+			JWTKey: "ImsKLIZXipqsHJKo_e3z",
+			Port:   "4000",
+		},
+	}
+	assert.Equal(expected, config)
 }
