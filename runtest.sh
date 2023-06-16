ca8e731bf9dec9a977cc642686379f9415b6399a
Kevin <vargaskevv@gmail.com>
2023-06-15 20:06:54 -0400

    Update runtest.sh to ignore some directories

--- runtest.sh
+++ runtest.sh
@@ -1,1 +1,2 @@
-DB_NAME=dbtest DB_HOST=hosttest DB_PORT=porttest DB_USER=usertest DB_PASSWORD=passtest DB_SSL_MODE=disable APP_PORT=4000 go test ./...
+DB_NAME=dbtest DB_HOST=hosttest DB_PORT=porttest DB_USER=usertest DB_PASSWORD=passtest DB_SSL_MODE=disable APP_PORT=4000
+go test $(go list ./... | grep -v /ent | grep -v /staticdata | grep -v /extra)
