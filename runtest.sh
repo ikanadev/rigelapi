54293fd14b9980de9825bfb84b78644cf3e674c3
Kevin <vargaskevv@gmail.com>
2023-06-17 03:38:26 -0400

    Update test script to use gotest

--- runtest.sh
+++ runtest.sh
@@ -5,4 +5,8 @@ DB_USER=usertest \
 DB_PASSWORD=passtest \
 DB_SSL_MODE=disable \
 APP_PORT=4000 \
-go test $(go list ./... | grep -v /ent | grep -v /staticdata | grep -v /extra | grep -v /database) -cover
+gotest $(go list ./... | grep -v /ent | grep -v /staticdata | grep -v /extra | grep -v /database) -coverprofile=cover.out
+echo "#######TOTAL######"
+echo ""
+go tool cover -func=cover.out
+echo ""
